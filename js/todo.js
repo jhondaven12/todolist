d02949c0131d9864e55cc8b30a1b6a20406877aa
jhondaven12 <78583928+jhondaven12@users.noreply.github.com>
2023-09-02 16:34:41 +0800

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -1,34 +1,37 @@
-const todoStorage = sessionStorage.getItem('todoStorage');
+const users = document.getElementById('todo-items');
+const TodoInput = document.getElementById('todoInput');
+const TotalItems = document.getElementById('totalItems');
+const TodoStorage = sessionStorage.getItem('TodoStorage');
 
-let todoList;
+let TodoList;
 
-todoStorage === null? todoList = [] : todoList = JSON.parse(todoStorage);
+TodoStorage === null ? TodoList = [] : TodoList = JSON.parse(TodoStorage);
 
-//Get Todolist Data
-function todoELement() { 
+//Display List and Total list
+function DisplayList() {
     let str = '';
 
-    for(let i = 0; i < todoList.length; i++) {
+    for (let i = 0; i < TodoList.length; i++) {
         str += `
         <div class="todolist active" data-class>
-        <label class='boxContainer' for="${todoList[i].id}">
-        <p>${todoList[i].text}</p>
-        <input type="checkbox" id="${todoList[i].id}" data-input>
-        <span class="checkmark"></span>
-        </label>
-        <div class="deltodoBtn" onclick="deltodo(${i})">
-        <img src="./images/icon-cross.svg">
+            <label class='boxContainer' for="${TodoList[i].id}">
+                <p>${TodoList[i].todo}</p>
+                <input type="checkbox" id="${TodoList[i].id}" data-input>
+                <span class="checkmark"></span>
+            </label>
+            <div class="deltodoBtn" onclick="Deltodo(${i})">
+                <img src="./images/icon-cross.svg">
+            </div>
         </div>
-        </div>
-        `;
+        `
     }
 
-    document.getElementById('todo-items').innerHTML = str;
-    
+    users.innerHTML = str;
+    TotalItems.innerHTML = `${TodoList.length} Items left`;
 }
-todoELement();
 
-//Checkboxes
+DisplayList();
+
 function checkBoxes(){
     var boxes = document.querySelectorAll("input[type='checkbox']");
     var todoClass = document.querySelectorAll('[data-class]');
@@ -74,118 +77,72 @@ function checkBoxes(){
 }
 checkBoxes();
 
-let todoInput = document.getElementById('todoInput');
-let addboxlist = document.getElementById('addboxlist');
+//Click Enter if input is not empty Submit Value
+TodoInput.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter'){
+        if(TodoInput.value.trim() === ''){
+            alert('Input is Empty')
+        } else {
+            AddTodo();
+            TodoInput.value = '';
+        }
+    }
+})
 
-todoInput.onkeypress = function(e) {
+//Add List
+function AddTodo() {
+    let RandomID = Math.floor(1 + Math.random() * 0x10000).toString(16).substring(1);
+    let InputValue = TodoInput.value.trim();
 
-    if(e.key === 'Enter') {
-        if(todoInput.value === '') { 
-            addboxlist.style.border = '2px solid red';
-        } else { 
-            createTodo();
-            addboxlist.style.border = '2px solid transparent';
-            todoInput.value = '';
+    TodoList.push({
+        id: RandomID,
+        todo: InputValue
+    })
+    myFunctions();
+}
+
+//Remove selected List
+function Deltodo(index){
+    TodoList.splice(index, 1);
+    myFunctions();
+}
+
+function myFunctions(){
+    DisplayList();
+    checkBoxes();
+    filterSelection('all');
+    sessionStorage.setItem('TodoStorage', JSON.stringify(TodoList));
+}
+
+function filterSelection(c) {
+    const elements = document.getElementsByClassName('todolist');
+    
+    for (const element of elements) {
+        if (c === 'all' || element.classList.contains(c)) {
+            addClass(element, 'show');
+        } else {
+            removeClass(element, 'show');
         }
     }
 }
-
-//Add list to Storage
-function createTodo(){
-
-    //Generate Random Id
-    let id = () => {
-        return Math.floor(1 + Math.random() * 0x10000)
-        .toString(16)
-        .substring(1);
-    }
-
-    let txt = todoInput.value.trim();
-
-    todoList.push({
-        id: id(),
-        text: txt
-    });
-
-    totalItems.innerHTML = todoList.length + ' items left';
-
-    todoELement();
-    checkBoxes();
-    filterSelection('all');
-    sessionStorage.setItem('todoStorage', JSON.stringify(todoList));
-}
-
-//Delete list to storage
-function deltodo(n){
-
-    todoList.splice(n, 1);
-
-    totalItems.innerHTML = todoList.length + ' items left';
-    
-    todoELement();
-    checkBoxes();
-    filterSelection('all');
-    sessionStorage.setItem('todoStorage', JSON.stringify(todoList));   
-
-}
-
-//todolist total items
-let totalItems = document.getElementById('totalItems');
-totalItems.innerHTML = todoList.length + ' items left';
-
-//filter for todolist
 filterSelection('all');
 
-function filterSelection(c){
-    var x, i;
-    x = document.getElementsByClassName('todolist');
-    if(c == 'all') c = "";
-    for(i = 0; i < x.length; i++) {
-        removeClass(x[i], "show");
-        if(x[i].className.indexOf(c) > -1) addClass(x[i], "show");
-    }
-}
-
-//Add Class to element
 function addClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for(i = 0; i < arr2.length; i++){
-        if(arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
+    const classesToAdd = name.split(' ');
+
+    for (const className of classesToAdd) {
+        if (!element.classList.contains(className)) {
+            element.classList.add(className);
         }
     }
 }
 
-//Remove Class to element 
 function removeClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for(i = 0; i < arr2.length; i++) {
-        while(arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
+    const classesToRemove = name.split(' ');
+
+    for (const className of classesToRemove) {
+        if (element.classList.contains(className)) {
+            element.classList.remove(className);
         }
     }
-    element.className = arr1.join(" ");
 }
-
-/*Delete All Checked List
-function clearAllComplete() {
-    const todoClass = [...document.querySelectorAll('[data-class]')];
-
-    todoClass.forEach((value, index) => {
-        if(value.classList.contains('complete')) {
-            todoList.splice(index, 1);
-
-            totalItems.innerHTML = todoList.length + ' items left';
-            
-            todoELement();
-            checkBoxes();
-            filterSelection('all');
-            sessionStorage.setItem('todoStorage', JSON.stringify(todoList));   
-        }
-    })
-}
-*/
